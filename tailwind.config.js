fa125e7b65f0b4b7127de0595ff7e18093762e46
gpetersonrust <gpeterson@rustcollege.edu>
2023-06-21 07:48:04 -0500

    Updated Subfolder and Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
- 
 const plugin = require('tailwindcss/plugin');
-const php_files = require('./php_glob'); 
-const { myUtilities, myComponents, myBase } = require('./library/tailwind/app/tailwind-plugin');
- 
- 
+const php_files = require('./php_glob');
+const {
+  myUtilities,
+  myComponents,
+  myBase,
+} = require('./library/tailwind-components/app/tailwind-plugin');
+
 module.exports = {
   mode: 'jit',
 
@@ -13,23 +15,24 @@ module.exports = {
     extend: {
       colors: {
         app: {
-          primary: '#2E3192',
-          secondary: '#0066B3',
+          primary: '#f91942',
+          ['primary-light']: '#ff395d',
+          light: '#5c5c5c',
+          secondary: '#f919420f',
           tertiary: '#FCB316',
-          quaternary: '#828282',
-          raven: '#132153;',
-          white: '#FFFFFF',
+          confirm: '#54ba1d',
+          raven: '#333333cc;',
+          dark: '#1b1b1b',
+          ['light-grey']: '#fafafa',
         },
       },
     },
   },
   plugins: [
     plugin(function ({ addUtilities, addComponents, addBase }) {
-      addUtilities( myUtilities , ['responsive', 'hover', 'focus', 'active']);
+      addUtilities(myUtilities, ['responsive', 'hover', 'focus', 'active']);
       addBase(myBase, ['responsive', 'hover', 'focus', 'active']);
-      addComponents( myComponents , ['responsive', 'hover', 'focus', 'active']);
+      addComponents(myComponents, ['responsive', 'hover', 'focus', 'active']);
     }),
   ],
 };
-
- 
