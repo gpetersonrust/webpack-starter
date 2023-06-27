3468a521ce3350532b0b502bbaa4fd2569d67536
gpetersonrust <gpeterson@rustcollege.edu>
2023-06-27 15:56:00 -0500

    Updated Gulp and Global file

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const gulp = require('gulp');
 const glob = require('glob');
 const browserSync = require('browser-sync');
 const { exec } = require('child_process');
- const { file_path, tailwind_dir } = require('./library/constants/global');
+const { file_path, tailwind_dir, footer_file, dev_js_file, line_in_footer_to_replace } = require('./library/constants/global');
 const find_files_recursively = require('./library/utilities/FS/find_files_recursively');
- 
+const browserify = require('gulp-browserify');
+const inject = require('gulp-inject');
+const { readFileSync, writeFileSync } = require('fs');
 const paths = glob.sync(file_path + '/**/**/*');
- 
- 
- 
+let new_footer_php = '';
+let footer_php;
+let dev_js_text;
+let script;
 const tailwind_customizer_files = find_files_recursively(
   tailwind_dir,
   [],
@@ -16,56 +19,92 @@ const tailwind_customizer_files = find_files_recursively(
   '.json$'
 );
 
-
 // check if system is mac
 const isMac = process.platform === 'darwin';
 
 // check if system is windows
 const isWindows = process.platform === 'win32';
 
-
-
 let proxy =
-  (isMac && 'http://localhost:8888/listeo') ||
-  (isWindows && 'http://localhost:8888/listeo');
+  (isMac && 'http://localhost:8888/task-manager') ||
+  (isWindows && 'http://localhost/task-manager');
 
 function watch() {
   browserSync.init({
     proxy,
-    port: 800,
+    port: 890,
+    scrollProportionally: false,
   });
 
   gulp.watch(paths).on('change', (path) => {
     browserSync.reload();
-  
-     
- 
-     
-
-    // exec("git pull origin master", (err, stdout, stderr) => {
-    //   if (err) {
-    //     console.error(err);
-    //     return;
-    //   }
-
-    //   console.log(stdout);
-    // });
   });
 
   gulp.watch(tailwind_customizer_files).on('change', () => {
     // change working directory to library/tailwind
-    process.chdir( tailwind_dir);
+    process.chdir(tailwind_dir);
 
     // run node update-tailwind.js
     exec('node update-tailwind.js', (err, stdout, stderr) => {
-     console.log('====================================');
-     console.log('updating');
-     console.log('====================================');
+      console.log('====================================');
+      console.log('updating');
+      console.log('====================================');
       if (err) {
         console.error(err);
         return;
       }
     });
   });
+
+
+
+   addScriptToFooter(footer_file, dev_js_file, line_in_footer_to_replace);
+ 
+
 }
-exports.default = watch;
+
+function addScriptToFooter(
+  footer_file,
+  dev_js_file,
+  line_in_footer_to_replace
+) {
+  const footer_php = readFileSync(footer_file, 'utf8');
+  const dev_js_text = readFileSync(dev_js_file, 'utf8');
+    script = `<script>\n${dev_js_text}</script>\n`;
+
+  // Check if script is already in footer
+  if (footer_php.includes(dev_js_text)) {
+    return;
+  }
+
+  
+    line_to_add_above = line_in_footer_to_replace;
+    new_footer_php = footer_php.replace(
+    line_to_add_above,
+    script + line_to_add_above
+  );
+
+  writeFileSync(footer_file, new_footer_php, 'utf8');
+}
+ 
+ 
+
+function cleanup() {
+    let file_php = readFileSync(footer_file, 'utf8');
+    let new_file_php = file_php.replace(script, '');
+
+    
+    writeFileSync(footer_file, new_file_php, 'utf8');
+
+
+  
+     
+}
+
+process.on('SIGINT', () => {
+  cleanup();
+  process.exit();
+ 
+});
+
+exports.default = gulp.series(watch);
